483b8c2377715d2c752a44b3c655c493f79840fc
lize <756725284@qq.com>
2019-08-28 10:27:15 +0800

    基础知识

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,31 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:'Codewhy',
+    age:18,
+    students:[
+      {id:110,name:'kobe',age:30},
+      { id: 110, name: 'james', age: 30 },
+      { id: 110, name: 'curry', age: 30 }
+    ],
+    counter:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick(){
+    //1.错误做法，界面不会刷新
+    // this.data.counter++;
+    // console.log(this.data.counter)
+    //2.this.setData()
+    this.setData({
+      counter:this.data.counter+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubTraction(){
+    this.setData({
+      counter:this.data.counter-1
+    })
   }
+
 })
+
+//编程范式：
+//1.命令式编程：原生操作Dom
+//2.声明式编程：Vue/React/Angular
